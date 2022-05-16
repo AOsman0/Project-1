8222879189766c244c7ff7a5fa45691c80c55c03
Your Name <you@example.com>
2022-05-16 14:40:31 +0100

    function to handle submission

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,28 @@
 //TODO find a way to link home page to search page
+const mainElement = document.getElementById("main");
+const API_KEY = "AIzaSyAb1sWH5SP_pa3SpuWv9TXLKXk9X2NWwFE";
+let title = "";
+let author = "";
+let image = "";
+let publisher = "";
+let description = "";
+
+// function to handle form submission
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  //get text input
+  let search = document.getElementById("input-text").value;
+  console.log(search);
+  //validate
+  if (search) {
+    // build object with full name and results
+    console.log("good search");
+
+    //clear feedbackResults
+  } else {
+    alert("enter valid search");
+  }
+};
 
 const onLoad = () => {
   //initialise feedback results
