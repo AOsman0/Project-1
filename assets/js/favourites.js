c0989e6e04806aee58d0d74cce353fce6231094e
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-05-17 15:28:37 +0100

    add event listeners added for books and quotes

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -1,22 +1,35 @@
 
 
 const bannerElement = document.getElementById('banner');
+const bookElement = document.getElementById('book-btn');
+const quotesElement = document.getElementById('quotes-btn');
+
 
 const onload =  () => {
     $("#banner").append(`     <div class="button-container">
-    <a class="fav-button waves-effect waves-light btn-large"
-      ><i class="material-icons right">cloud</i>Saved quotes</a
+    <a id="quotes-btn" class="fav-button waves-effect waves-light btn-large"
+      ><i  class="material-icons right">cloud</i>Saved quotes</a
     >
-    <a class="fav-button waves-effect waves-light btn-large"
+    <a id="book-btn" class="fav-button waves-effect waves-light btn-large"
       ><i class="material-icons right">book</i>Saved books</a
     >
   </div>
   <div class="favourite-title">
     <h1 class="favo">FAVOURITES</h1>
   </div>`)
+  $("#book-btn").click(renderSavedBooks);
+  $("#quotes-btn").click(renderSavedQuotes);
 
 }
 
+const renderSavedBooks = () => {
+    
+
+}
+
+const renderSavedQuotes = () => {
+    console.log('bttn clicked')
+}
 
 
 
