08ef4bc2d456cdb6c9ce1ae1f40fd815337c4bd5
Your Name <you@example.com>
2022-05-17 21:58:36 +0100

    javascript complete for rendering and saving books

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,6 @@
 //TODO find a way to link home page to search page
 const mainElement = document.getElementById("main");
-const resultsSection = document.getElementById("results");
-const addButton = document.getElementById("add");
+const resultsSection = document.getElementById("cards");
 const API_KEY = "AIzaSyAb1sWH5SP_pa3SpuWv9TXLKXk9X2NWwFE";
 let title = "";
 let author = "";
@@ -10,6 +9,8 @@ let publisher = "";
 let description = "";
 let bookLink = "";
 let trimmedDescription = "";
+let currentSearchResults = [];
+let favoriteBookList = [];
 
 // function to handle form submission
 const handleFormSubmit = (event) => {
@@ -18,6 +19,7 @@ const handleFormSubmit = (event) => {
   let search = document.getElementById("input-text").value;
   console.log(search);
   //validate
+
   if (search) {
     // build object with full name and results
     console.log("good search");
@@ -42,8 +44,14 @@ const fetchBookData = () => {
     .then(function (result) {
       items = result.items;
       console.log(items);
+      if (document.getElementById("movie-cards") !== null) {
+        document.getElementById("movie-cards").remove();
+      }
       // gather the info needed for 5 cards
+      $("#results").append(`<div id="movie-cards" class="results"></div>`);
       for (i = 0; i < 10; i++) {
+        // add this whole object to the currentSearchResults array
+        currentSearchResults.push(items[i]);
         // from the response cherry pick Title, AUTHOR, PUBLISHER DESCRIPTION and IMAGE
         title = items[i].volumeInfo.title;
         console.log(title);
@@ -52,14 +60,20 @@ const fetchBookData = () => {
         publisher = items[i].volumeInfo.publisher;
         console.log(publisher);
         if (!items[i].volumeInfo.imageLinks) {
-          image = ".assets / images / placeholder.png";
+          image = ".assets/images/placeholder.png";
         } else {
           image = items[i].volumeInfo.imageLinks.thumbnail;
         }
         console.log(image);
         description = items[i].volumeInfo.description;
-        trimmedDescription = description.slice(0, 100);
-        console.log(trimmedDescription);
+        // variable to limit description character count
+        if (description !== null && description !== undefined) {
+          trimmedDescription = description.slice(0, 100);
+          console.log(trimmedDescription);
+        } else {
+          trimmedDescription = "";
+        }
+
         bookLink = items[i].volumeInfo.previewLink;
         console.log(bookLink);
         let bookResults = [title, author, publisher, image, trimmedDescription];
@@ -67,6 +81,8 @@ const fetchBookData = () => {
         // render results card
         renderResult(bookResults);
       }
+      console.log(currentSearchResults);
+      // now that we are done rendering the cards, lets push the currentSearchResult to local storage
     }),
     function (error) {
       console.log(error);
@@ -91,19 +107,19 @@ const renderResultsBanner = () => {
     </section>`);
   form.addEventListener("submit", handleFormSubmit);
 };
+
 // function to render results banner
-// TODO limit characters in description
 const renderResult = () => {
-  $("#results").append(`<div class="row">
+  $("#movie-cards").append(`<div class="row">
   <div class="col s12 m6">
     <div class="card">
-      <div class="card-image">
+      <div class="card-image image-size">
         <img
           src=${image}
         />
         <span class="card-title">${title}</span>
-        <a class="btn-floating halfway-fab waves-effect waves-light red"
-          ><i class="material-icons" id="add${i}">add</i></a
+        <a  class="btn-floating halfway-fab waves-effect waves-light red"
+          ><i id="${i}" class="material-icons" >add</i></a
         >
       </div>
       <div class="card-content">
@@ -120,15 +136,52 @@ const renderResult = () => {
     </div>
   </div>
 </div>`);
-  $("#add").click(addButtonClick);
+  $(`#${i}`).click(addButtonClick);
+};
+// function to store answer in local storage
+const storeInLS = (key, value) => {
+  //get feedbackResults from LS
+  const arrayFromLS = JSON.parse(localStorage.getItem(key));
+
+  //push answer in to array
+  arrayFromLS.push(value);
+
+  //set feedbackResults in LS
+  localStorage.setItem(key, JSON.stringify(arrayFromLS));
 };
 const initialiseLocalStorage = () => {
-  console.log("local storage needs to be done");
+  const favouriteBooks = JSON.parse(localStorage.getItem("favoriteBook"));
+  if (!favouriteBooks) {
+    localStorage.setItem("favoriteBook", JSON.stringify([]));
+  }
 };
-//function to add to favourite storage
+//function to add to favourite list
 // TODO FIND A WAY TO TARGET ONLY ONE CARD ON EACH CLICK
-const addButtonClick = () => {
-  console.log("add-button-clicked" + addButton);
+const addButtonClick = (event) => {
+  const target = event.target;
+  console.log("event.target: " + target);
+  const currentTarget = event.currentTarget;
+  console.log("event.currentTarget: " + currentTarget);
+  console.log("target.tagName: " + target.tagName);
+  console.log("target.id: " + target.id);
+  const cardNum = target.id;
+  console.log(cardNum);
+
+  // we want to add this movie to the favoriteMovieList list
+  // step 1:
+  // fetch the existing favoriteMovieList from the local storage
+  const savedBook = currentSearchResults[cardNum];
+  console.log(savedBook);
+
+  // check if the last came back as null or undefined
+
+  // append this movie to the favoriteMovieList
+  storeInLS("favoriteBook", savedBook);
+  // (optional) check if this movie already exists in the movie list
+  // if yes, don't add it again
+
+  // step 2:
+  // save favoriteMovieList again in the local storage
 };
 const onLoad = () => {
   //initialise feedback results
