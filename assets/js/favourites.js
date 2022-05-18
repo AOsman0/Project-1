b538b5a5a11f4b67759c3583d9f0b05732f78bae
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-05-18 13:12:00 +0100

    rendered all saved books

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -3,6 +3,12 @@ const bookElement = document.getElementById("book-btn");
 const quotesElement = document.getElementById("quotes-btn");
 const favouriteSection = document.getElementById("book-favourites");
 
+let author = '';
+let title = '';
+let image = '';
+let bookLink = '';
+
+
 const onload = () => {
   $("#banner").append(`     <div class="button-container">
     <a id="quotes-btn" class="fav-button waves-effect waves-light btn-large"
@@ -21,12 +27,38 @@ const onload = () => {
 
 const renderSavedBooks = () => {
   //fetch data from local storage
-  const arrayFromLS = JSON.parse(localStorage.getItem('favouriteBook'));
-  console.log (arrayFromLS )
+  const items = JSON.parse(localStorage.getItem('favoriteBook'));
+  console.log (items )
 
  // own comments: i am going to get the data and 
 
   //set forloop for number of sets in the local storage
+  for (i = 0; i < items.length; i++) {
+    // add this whole object to the currentSearchResults array
+    
+    // from the response cherry pick Title, AUTHOR, PUBLISHER DESCRIPTION and IMAGE
+    title = items[i].volumeInfo.title;
+    console.log(title);
+    author = items[i].volumeInfo.authors;
+    console.log(author);
+    bookLink = items[i].volumeInfo.previewLink;
+    console.log(bookLink);
+    
+    
+    if (!items[i].volumeInfo.imageLinks) {
+      image = ".assets/images/placeholder.png";
+    } else {
+      image = items[i].volumeInfo.imageLinks.thumbnail;
+    }
+    let bookResults = [title, author, image,];
+    console.log("book results" + bookResults);
+    // render results card
+    renderFavoriteBooks(bookResults);
+    
+  }
+};
+const renderFavoriteBooks = () => {
+
 
   // if saved quotes is rendered, clear it from page.
 
@@ -34,12 +66,12 @@ const renderSavedBooks = () => {
   $("#book-favourites").append(` <div class="card-container col s12 m7">
     <div class="card horizontal">
       <div class="card-image">
-        <img class="image" src="./assets/images/mountain.jpg" />
+        <img class="image" src=${image} />
       </div>
       <div class="card-stacked">
         <div class="card-content">
-          <p>title</p>
-          <p>author</p>
+          <p>title: ${title}</p>
+          <p>author: ${author}</p>
         </div>
 
         <div class="card-action">
